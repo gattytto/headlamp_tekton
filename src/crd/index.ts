b96a5e497b281a00bff99b0dee778371fef01c14
asd <asd@asd.com>
2026-05-25 19:52:46 -0300

    barrel exports

---
+++ src/crd/index.ts
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: EPL-2.0
+
+export * from './clusterinterceptor';
+export * from './eventlistener';
+export * from './pipeline';
+export * from './pipelinerun';
+export * from './task';
+export * from './taskrun';
+export * from './trigger';
+export * from './triggertemplate';
